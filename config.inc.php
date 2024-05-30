041cb253f405996ec8effda0d0b118ac4059a150
viskikolos <161343052+viskikolos@users.noreply.github.com>
2024-05-30 15:02:12 +0200

    Add files via upload

--- config.inc.php
+++ config.inc.php
@@ -16,12 +16,14 @@ $lablec = array(
 );
 
 $oldalak = array(
-	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap'),
-    'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria'),
-	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás'),
-	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
-	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek'),
-    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat')
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'üzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
 );
 
 $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
